9818651547102f3c89bee1a794a567c596fc6eec
Rodrigo Renck <renckrodrigo030@gmail.com>
2023-03-22 18:23:19 -0300

    exercicio 1 - factory e singleton

---
+++ src/AppExercicio1.java
@@ -0,0 +1,9 @@
+public class AppExercicio1 {
+
+    public static void main(String[] args) {
+        var collectionFactory = CollectionFactory.getInstance();
+
+        var result = collectionFactory.getCollection(100, true, true, false);
+
+    }
+}
---
+++ src/CollectionFactory.java
@@ -0,0 +1,33 @@
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.*;
+
+public class CollectionFactory{
+
+    public static CollectionFactory factory = null;
+
+    private CollectionFactory(){
+
+    }
+
+    //se passar 0 eh porque nao importa o tamanho
+    public <T extends Comparable<T>> Collection<T> getCollection(int size, boolean order, boolean canRepeat, boolean naturalOrder) {
+        if (size != 0 && order && canRepeat && !naturalOrder) {
+            return new ArrayList<T>();
+        } else if (size == 0 && order && canRepeat && !naturalOrder) {
+            return new LinkedList<T>();
+        } else if (size <= 0 && !order && !canRepeat && !naturalOrder) {
+            return new HashSet<T>();
+        } else if (size <= 0 && !order && !canRepeat) {
+            return new TreeSet<T>();
+        }
+        return null;
+    }
+
+    public static CollectionFactory getInstance(){
+        if(factory == null){
+            factory = new CollectionFactory();
+        }
+        return factory;
+    }
+
+}
